8fbd6d17564903a30b1b2f16d727f70a66ae8089
Faadil Torou <ftorou@gmail.com>
2023-02-05 19:03:13 +0100

    modif HUD

--- Code/Levelboss.gd
+++ Code/Levelboss.gd
@@ -4,4 +4,9 @@ var enemies_left
 
 func _process(delta):
 	enemies_left = get_tree().get_nodes_in_group("Enemies").size()
+	set_score(enemies_left)
 	print(enemies_left)
+
+func set_score(val):
+	enemies_left = val
+	get_node("HUD/enemies_lft").set_text("Enemies left : "+ str(enemies_left))
